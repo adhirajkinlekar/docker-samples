e94120fde52822310624203263559974355ab429
Adhiraj Kinlekar <adhirajkinlekar@outlook.com>
2025-04-06 10:31:53 +0530

    fix format

--- node-kafka/email-service/index.js
+++ node-kafka/email-service/index.js
@@ -1,40 +1,40 @@
 const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-  clientId: 'email-service',
-  brokers: ['kafka:9092'],
+    clientId: 'email-service',
+    brokers: ['kafka:9092'],
 });
 
 const consumer = kafka.consumer({ groupId: 'email-group' });
 
 const run = async () => {
 
-  await consumer.connect();
+    await consumer.connect();
 
-  await consumer.subscribe({ topic: 'user-events', fromBeginning: true });
+    await consumer.subscribe({ topic: 'user-events', fromBeginning: true });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
 
-      const parsed = JSON.parse(message.value.toString());
-      
-      const { eventType, data } = parsed; 
+            const parsed = JSON.parse(message.value.toString());
 
-      switch (eventType) {
-        case 'user-registered':
-          console.log(`📧 Send welcome email to ${data.email}`);
-          break;
-        case 'user-updated':
-          console.log(`🔁 Send update notification for ${data.email}`);
-          break;
-        case 'user-deleted':
-          console.log(`❌ Notify about deletion of ${data.email}`);
-          break;
-        default:
-          console.log(`⚠️ Unknown event type: ${eventType}`);
-      }
-    },
-  });
+            const { eventType, data } = parsed;
+
+            switch (eventType) {
+                case 'user-registered':
+                    console.log(`📧 Send welcome email to ${data.email}`);
+                    break;
+                case 'user-updated':
+                    console.log(`🔁 Send update notification for ${data.email}`);
+                    break;
+                case 'user-deleted':
+                    console.log(`❌ Notify about deletion of ${data.email}`);
+                    break;
+                default:
+                    console.log(`⚠️ Unknown event type: ${eventType}`);
+            }
+        },
+    });
 };
 
 run().catch(console.error);
--- node-kafka/user-service/index.js
+++ node-kafka/user-service/index.js
@@ -5,36 +5,36 @@ const app = express();
 app.use(express.json());
 
 const kafka = new Kafka({
-  clientId: 'user-service',
-  brokers: ['kafka:9092'],
+    clientId: 'user-service',
+    brokers: ['kafka:9092'],
 });
 
 const producer = kafka.producer();
 
 const run = async () => {
-  await producer.connect();
+    await producer.connect();
 
-  app.post('/register', async (req, res) => {
-    const user = req.body;
+    app.post('/register', async (req, res) => {
+        const user = req.body;
 
-    if(!user.email || !user.name) return res.status(422).send("User name of email are invalid");
+        if (!user.email || !user.name) return res.status(422).send("User name of email are invalid");
 
-    await producer.send({
-        topic: 'user-events',
-        messages: [
-          {
-            value: JSON.stringify({
-              eventType: 'user-registered',
-              data: user,
-            }),
-          },
-        ],
-      });
+        await producer.send({
+            topic: 'user-events',
+            messages: [
+                {
+                    value: JSON.stringify({
+                        eventType: 'user-registered',
+                        data: user,
+                    }),
+                },
+            ],
+        });
 
-    res.send('User registered and event published');
-  });
+        res.send('User registered and event published');
+    });
 
-  app.listen(3000, () => console.log('User service on port 3000'));
+    app.listen(3000, () => console.log('User service on port 3000'));
 };
 
 run().catch(console.error);
